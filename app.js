cd9a95c23f04b9052d0162670439f7bbb8c78bda
Tricia <chenaicirt@gmail.com>
2019-01-29 01:34:42 -0500

    fixed npm start issue

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var http = require('http');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var server = http.createServer(function(request, response) {
 
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
