969fa5197b0c7126d0bf520e5b6e8ec0ae031b08
Tricia <chenaicirt@gmail.com>
2019-01-29 16:14:39 -0500

    removed myapp and changed server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-const port = 3000;
+const port = 80;
 const server = app_1.default.listen(port, "0.0.0.0", () => {
     console.log(`Example app listening on port ${port}`);
 });
