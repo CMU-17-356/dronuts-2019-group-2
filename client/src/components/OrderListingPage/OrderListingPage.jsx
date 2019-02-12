18eda336773c6fb736254aad8eb18d9674cbdd47
Tricia <chenaicirt@gmail.com>
2019-02-12 17:09:54 -0500

    tried to pad

--- client/src/components/OrderListingPage/OrderListingPage.jsx
+++ client/src/components/OrderListingPage/OrderListingPage.jsx
@@ -9,7 +9,7 @@ class OrderListingPage extends Component {
       <div className="olp">
 <Container>
   <Row>
-    <Col>          
+     <Col md={{ padding: 20}}>          
       <Order
           id="1"
           drone="1"
@@ -19,25 +19,50 @@ class OrderListingPage extends Component {
       />
     </Col>
 
-    <Col>      
+     <Col md={{ padding: 20}}>      
         <Order
             id="2"
             drone="2"
-            donuts={[{id: 1, name:'Glazed', quantity: 3}]}
+            donuts={[{id: 1, name:'Glazed', quantity: 3}, {id: 3, name:'Boston Cream', quantity: 1}]}
             address="5000 Forbes Avenue"
           />
     </Col>
-    <Col>
+     <Col md={{ padding: 20}}>
           <Order
             id="3"
             drone="3"
-            donuts={[{id: 2, name:'Chocolate Frosted', quantity: 1}]}
+            donuts={[{id: 2, name:'Chocolate Frosted', quantity: 1},{id: 1, name:'Glazed', quantity: 2},]}
             address="404 Wilkins Avenue"
           />
     </Col>
   </Row>
-
   <Row>
+   <Col md={{ padding: 20}}>
+            <Order
+            id="3"
+            drone="3"
+            donuts={[{id: 1, name:'Glazed', quantity: 1}, {id: 2, name:'Chocolate Frosted', quantity: 2},]}
+            address="400 Wilkins Avenue"
+          />
+  </Col>
+  
+   <Col md={{ padding: 20}}>
+
+            <Order
+            id="3"
+            drone="3"
+            donuts={[{id: 1, name:'Glazed', quantity: 1}, {id: 2, name:'Chocolate Frosted', quantity: 2},]}
+            address="400 Wilkins Avenue"
+          />
+  </Col>
+         <Col md={{ padding: 20}}>
+          <Order
+            id="3"
+            drone="3"
+            donuts={[{id: 1, name:'Glazed', quantity: 1}, {id: 2, name:'Chocolate Frosted', quantity: 2},]}
+            address="400 Wilkins Avenue"
+          />
+    </Col>
   </Row>
 </Container>
 
