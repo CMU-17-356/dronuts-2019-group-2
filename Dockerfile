28f4eb1baf697d45dae4b660394ffcb74c0169ae
Tricia <chenaicirt@gmail.com>
2019-02-11 23:29:21 -0500

    changed up docker files again

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,30 @@
-# Setup and build the client
+# setup react build
+FROM node:8-alpine as client
+MAINTAINER dronuts_4
 
-FROM node:9.4.0-alpine as client
-
-WORKDIR /usr/app/client/
+# Change working directory
+WORKDIR /usr/app/client
 COPY client/package*.json ./
-RUN npm install -qy
+RUN npm install
+# Issue in newest ajv version
+RUN npm uninstall ajv
+RUN npm install ajv@6.8.1
 COPY client/ ./
 RUN npm run build
 
-
-# Setup the server
-
-FROM node:9.4.0-alpine
+# setup express
+FROM node:8-alpine
 
 WORKDIR /usr/app/
 COPY --from=client /usr/app/client/build/ ./client/build/
 
 WORKDIR /usr/app/server/
-COPY server/package*.json ./
-RUN npm install -qy
-COPY server/ ./
+COPY express/package*.json ./
+RUN npm install
+COPY express/. .
 
-ENV PORT 80
+# Copy App Source
+#TODO Run any build scripts here
 
 EXPOSE 80
-
-CMD ["npm", "start"]
+CMD [ "npm", "start" ]
--- server/src/index.js
+++ server/src/index.js
@@ -1,33 +1,23 @@
-'use strict';
+var express = require('express')
+var path = require('path')
+var cors = require('cors')
+var app = express()
 
-const express = require('express');
-const path = require('path');
+const port = 80 
 
-// Constants
-const PORT = 80;
-// const HOST = '104.42.146.209';
+app.use(cors())
+const CLIENT_BUILD_PATH = path.join(__dirname, '../client/build');
+app.use(express.static(CLIENT_BUILD_PATH))
 
-const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
-
-// App
-const app = express();
-
-// Static files
-app.use(express.static(CLIENT_BUILD_PATH));
-
-// API
-app.get('/api', (req, res) => {
-  res.set('Content-Type', 'application/json');
-  let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
-  };
-  res.send(JSON.stringify(data, null, 2));
-});
-
-// All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
+app.get('/', function(request, response) {
   response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
-app.listen(PORT);
-console.log(`Running on http://localhost:${PORT}`);
+app.get('/message', (req, res) => {
+	console.log("hit the message API")
+	res.json({
+		message: "If you're seeing this, the Express API is working"
+	})
+})
+
+app.listen(port, () => console.log("listening on" +port));
