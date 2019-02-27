267f8eb1287ee66b8fc1290f36ab8a42c0bae700
Tricia <chenaicirt@gmail.com>
2019-02-26 20:29:38 -0500

    order functionality

--- client/src/components/OrderListingPage/Order.jsx
+++ client/src/components/OrderListingPage/Order.jsx
@@ -2,21 +2,44 @@ import React, { Component } from 'react';
 import {Button} from 'react-bootstrap';
 
 class Order extends Component {
-  onOrderClick() {
-    alert("Order sent!")
+  constructor () {
+    super();
+    this.state = {
+      isHidden: false,
+    };
+
+    this.onOrderClick = this.onOrderClick.bind(this);
+
+
   }
+
+  onOrderClick() {
+    this.setState({ isHidden: !this.state.isHidden });
+  }
+
+
   
   render() {
-    const { id, drone, donuts, address } = this.props;
+    const { id, drone, donuts, address, status } = this.props;
     const order = donuts.map((donut, key) =>
       <li key={donut.id}>
         {donut.name}: {donut.quantity}
       </li>
     )
     
+    if (this.state.isHidden) {
+      return (<div> </div>);
+
+    }
+    else {
+
+      if (status == "packing") {
     return (
       // <li id={id}>
+
+    
         <span>
+
           <div className="drone">
             <h3>Drone {drone}</h3>
           </div>          
@@ -30,12 +53,41 @@ class Order extends Component {
               Complete order
 
           </Button>
+   
+        
+               <br />
           <br />
-          <br />
-
         </span>
       // </li>
     );
+        }
+      else {
+
+          return (
+      // <li id={id}>
+
+    
+        <span>
+
+          <div className="drone">
+            <h3>Drone {drone}</h3>
+          </div>          
+          <div className="order">      
+            <h5>Deliver to: {address}</h5>
+            <ul>
+              {order}
+            </ul>
+          </div>
+   
+        
+               <br />
+          <br />
+        </span>
+      // </li>
+    );
+      }
+    }
+
   }
 }
 
